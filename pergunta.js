62ea622db6e244a0900ac48e569ea2ca2c4104c4
vinidomorro <160952347+vinidomorro@users.noreply.github.com>
2024-11-21 16:50:51 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,20 @@
+criacartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+criacartao(
+    'Programação',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de programação'
+)
+criacartao(
+    'Geografia',
+    'Qual a capital da França',
+    'Acapital da Frnça é Paris'
+)
+criacartao(
+    'Programação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
